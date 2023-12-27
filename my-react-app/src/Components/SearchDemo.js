ba4a7f41bfdd06afe9fd33f3f1fa1c5884c33c0a
mtuanvu <143334278+mtuanvu@users.noreply.github.com>
2023-12-28 00:47:03 +0700

    u

--- my-react-app/src/Components/SearchDemo.js
+++ my-react-app/src/Components/SearchDemo.js
@@ -1,23 +1,44 @@
 import React, { useState } from "react";
 import "./SearchDemo.css";
-import SearchIcon from "@material-ui/icons/Search";
+import { Search } from "@mui/icons-material";
 
 function SearchDemo({ placeholder, data }) {
   const [filteredData, setFilteredData] = useState([]);
 
+  const search = (str) => {
+    const result = data.filter((value) => {
+      return value.title.toLowerCase().includes(str.toLowerCase());
+    });
+    setFilteredData(result);
+  };
   return (
     <div className="search">
       <div className="search-input">
-        <input type="text" placeholder={placeholder} />
+        <input
+          type="text"
+          placeholder={placeholder}
+          onChange={(e) => {
+            if (e.target.value === "") {
+              setFilteredData([]);
+            } else {
+              search(e.target.value);
+            }
+          }}
+        />
         <div className="search-icon">
-          <SearchIcon />
+          <Search />
         </div>
       </div>
       {
         <div className="dataResult">
-          {data.map((value, key) => {
+          {filteredData.map((value, key) => {
             return (
-              <a className="dataItem" href={value.link} target="_blank">
+              <a
+                className="dataItem"
+                href={value.link}
+                target="_blank"
+                rel="noreferrer"
+              >
                 <p>{value.title}</p>
               </a>
             );
